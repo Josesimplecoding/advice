f829d56c2fddc746d30ce41f73049246f5bf9fdf
Josesimplecoding <Jose.salazar9877@gmail.com>
2022-03-10 18:35:33 -0600

    layout completed

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>Home Page</h1>
+    <div>
+        <input type="text" placeholder="Search for Advise ..." class="w-full mt-16 text-xl text-center rounded-full"/>
+        <div class="grid gap-16 pt-16 grid-col-2">
+            <p>
+                lore ipsum dolor sit amet consectetur adipisicing elit. Deserunt autem
+                reprehenderit iusto qui, dolor explicabo, quae beatae officia eveniet
+                itaque, quia magnam sunt quaerat? Vero necessitatibus impedit maiores nobis
+                aperiam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
+                inventore magni temporibus asperiores sunt nihil maiores excepturi, velit
+                neque et non autem unde optio quos aspernatur blanditiis in eligendi
+                repellat? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+                fuga aspernatur numquam blanditiis at consectetur magnam, voluptatem
+                sapiente voluptate adipisci dolore nesciunt dignissimos! Vel omnis, nulla
+                corporis deserunt consequuntur ratione?
+            </p>
+            <p>
+                lore ipsum dolor sit amet consectetur adipisicing elit. Deserunt autem
+                reprehenderit iusto qui, dolor explicabo, quae beatae officia eveniet
+                itaque, quia magnam sunt quaerat? Vero necessitatibus impedit maiores nobis
+                aperiam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
+                inventore magni temporibus asperiores sunt nihil maiores excepturi, velit
+                neque et non autem unde optio quos aspernatur blanditiis in eligendi
+                repellat? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+                fuga aspernatur numquam blanditiis at consectetur magnam, voluptatem
+                sapiente voluptate adipisci dolore nesciunt dignissimos! Vel omnis, nulla
+                corporis deserunt consequuntur ratione?
+            </p>
+            <p>
+                lore ipsum dolor sit amet consectetur adipisicing elit. Deserunt autem
+                reprehenderit iusto qui, dolor explicabo, quae beatae officia eveniet
+                itaque, quia magnam sunt quaerat? Vero necessitatibus impedit maiores nobis
+                aperiam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
+                inventore magni temporibus asperiores sunt nihil maiores excepturi, velit
+                neque et non autem unde optio quos aspernatur blanditiis in eligendi
+                repellat? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+                fuga aspernatur numquam blanditiis at consectetur magnam, voluptatem
+                sapiente voluptate adipisci dolore nesciunt dignissimos! Vel omnis, nulla
+                corporis deserunt consequuntur ratione?
+            </p>
+        </div>
+    </div>
 </template>
